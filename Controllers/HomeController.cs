3dd4b4f9ec637f19bbd0cd399c8e91cb3a34f582
Pushpasri M <127647687+pushpasri-M@users.noreply.github.com>
2025-07-29 10:14:01 +0530

    Add

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -21,13 +21,24 @@ namespace Registration.Controllers
 
         public ActionResult Contact()
         {
-           
-
+            
             return View();
         }
         public ActionResult FormData()
         {
-            return View();
+            string name = Request.Form["name"];
+            string email = Request.Form["email"];
+            string message = Request.Form["message"];
+            SqlConnection cont = new SqlConnection("Data source=GET-ME-OUT\\SQLEXPRESS;Database=MessageCS;Integrated Security=true");
+            cont.Open();
+            string query = "insert into MessageIF(username, email, messageUS) VALUES (@username, @email, @messageUS)";
+            SqlCommand comd = new SqlCommand(query, cont);
+            comd.Parameters.AddWithValue("@username", name);
+            comd.Parameters.AddWithValue("@email", email);
+            comd.Parameters.AddWithValue("@messageUS", message);
+            comd.ExecuteNonQuery();
+            cont.Close();
+             return RedirectToAction("Contact");
         }
     }
 }
